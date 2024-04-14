cdba9c92a5b0bc18a0f07e734d5260635f9db282
Pavol Vernarec <pver@paymorrow.com>
2024-04-14 20:13:17 +0200

    privacy policy and terms

--- src/app/(side)/privacy-policy/page.tsx
+++ src/app/(side)/privacy-policy/page.tsx
@@ -3,16 +3,137 @@ import { ArrowBigLeft } from 'lucide-react';
 import Link from 'next/link';
 
 export default function TermsPage() {
+  const WEBSITE_NAME = 'Website Name';
+  const WEBSITE_ADDRESS = 'Website Address';
+  const EMAIL = 'Email';
+
   return (
     <div className="p-10">
       <Link href="/" className={buttonVariants()}>
         <ArrowBigLeft className="w-6 h-6" />
         Back to home
       </Link>
-      <h1 className="text-2xl my-3">Policy for Thumbnail Feedbacks</h1>
+      <h1 className="text-2xl my-3">Privacy Policy</h1>
+      <p>Effective Date: 24/04/15</p>
+      <h2 className="text-lg my-3">Introduction</h2>
       <p className="text-md font-sans font-light">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum amet accusantium et minus, quo ad velit
-        reprehenderit totam harum id. Quisquam quam, tempore assumenda fuga rerum aut. Odit, non earum!
+        Welcome to {WEBSITE_NAME} <strong className="font-bold">(&quot;Website&quot;)</strong> provided by our company
+        PALIBATCHEE HOUSE s. r. o., with a registered seat at Klincova 35, 821 08 Bratislava, Slovak Republic,
+        registered within the Commercial Register of Municipal Court Bratislava III, Section: Sro, Insert No.: 114327/B{' '}
+        <strong className="font-bold">
+          (&ldquo;The Company&rdquo;, &ldquo;We&rdquo;, &ldquo;Us&rdquo;, &ldquo;Our&rdquo;)
+        </strong>
+        . We are committed to protecting the privacy of your data. This Privacy Policy explains how we collect, use,
+        protect, and share information and personal data of our users (&ldquo;You&rdquo;). Our processing of your
+        personal data is in compliance with the General Data Protection Regulation (GDPR) applicable to data processors
+        in the EU.
+      </p>
+      <p className="text-md font-sans font-light">Contact Details:</p>
+      <ul className="font-sans font-light list-disc ml-10">
+        <li>Company Name: PALIBATCHEE HOUSE s. r. o.</li>
+        <li>Address: Klincova 35, 821 08 Bratislava, Slovak Republic</li>
+        <li>Email: {EMAIL}</li>
+      </ul>
+      <h2 className="text-lg my-3">Data Collection and Types</h2>
+      <p className="text-md font-sans font-light">
+        We collect personal data that you provide to us when you use our services. This data includes:
+      </p>
+      <ul className="font-sans font-light list-disc ml-10">
+        <li>
+          <strong className="font-bold">Personal identification information:</strong> Name, email address, etc.
+        </li>
+        <li>
+          <strong className="font-bold">Usage data:</strong> Information on how you use our website and services.
+        </li>
+        <li>
+          <strong className="font-bold">Technical data:</strong> IP address, browser type and version, time zone
+          settings, and operating system.
+        </li>
+      </ul>
+      <p className="text-md font-sans font-light">Data is collected through:</p>
+      <ul className="font-sans font-light list-disc ml-10">
+        <li>Forms filled out by you on the Website</li>
+        <li>Cookies and similar tracking technologies</li>
+        <li>Other direct interactions as emails and contact forms</li>
+      </ul>
+      <h2 className="text-lg my-3">Legal Basis for Processing</h2>
+      <p className="text-md font-sans font-light">We process your personal data under the following legal bases:</p>
+      <ul className="font-sans font-light list-disc ml-10">
+        <li>
+          <strong className="font-bold">Consent:</strong> You have given clear consent for us to process your personal
+          data for specific purposes.
+        </li>
+        <li>
+          <strong className="font-bold">Contract:</strong> The processing is necessary for a contract you have with us.
+        </li>
+        <li>
+          <strong className="font-bold">Legal obligation:</strong> The processing is necessary for us to comply with the
+          law.
+        </li>
+        <li>
+          <strong className="font-bold">Legitimate interests:</strong> The processing is necessary for our legitimate
+          interests or the legitimate interests of a third party.
+        </li>
+      </ul>
+      <h2 className="text-lg my-3">Data Use</h2>
+      <p className="text-md font-sans font-light">We use your personal data to:</p>
+      <ul className="font-sans font-light list-disc ml-10">
+        <li>Provide and manage our services.</li>
+        <li>Communicate with you, including sending service-related information.</li>
+        <li>Improve and personalize our services.</li>
+        <li>Comply with legal requirements.</li>
+      </ul>
+      <h2 className="text-lg my-3">Data Transfer</h2>
+      <p className="text-md font-sans font-light">
+        Your personal data may be transferred to and stored at a destination outside the European Economic Area (EEA).
+        It may also be processed by staff operating outside the EEA who work for us or for one of our suppliers. We will
+        take all steps reasonably necessary to ensure that your data is treated securely and in accordance with this
+        privacy policy.
+      </p>
+      <h2 className="text-lg my-3">Data Retention</h2>
+      <p className="text-md font-sans font-light">
+        We retain personal data only for as long as necessary to fulfill the purposes we collected it for, including for
+        the purposes of satisfying any legal, accounting, or reporting requirements. The criteria we use to determine
+        retention periods include the length of time we have an ongoing relationship with you and whether there is a
+        legal obligation to which we are subject.
+      </p>
+      <h2 className="text-lg my-3">User Rights</h2>
+      <p className="text-md font-sans font-light">Under GDPR, you have the right to:</p>
+      <ul className="font-sans font-light list-disc ml-10">
+        <li>Access your personal data.</li>
+        <li>Correct any mistakes in your data.</li>
+        <li>Erase your personal data.</li>
+        <li>Object to or restrict processing.</li>
+        <li>Data portability.</li>
+      </ul>
+      <p className="text-md font-sans font-light">To exercise any of these rights, please contact us at {EMAIL}.</p>
+      <h2 className="text-lg my-3">Data Security</h2>
+      <p className="text-md font-sans font-light">
+        We have implemented appropriate security measures to prevent your personal data from being accidentally lost,
+        used, altered, disclosed, or accessed without authorization. We also allow access to your personal data only to
+        those employees and partners who have a business need to know such data.
+      </p>
+      <h2 className="text-lg my-3">Use of Cookies and Similar Technologies</h2>
+      <p className="text-md font-sans font-light">
+        We use cookies and similar tracking technologies to track the activity on our service and hold certain
+        information. You can instruct your browser to refuse all cookies or to indicate when a cookie is being sent.
+        However, if you do not accept cookies, you may not be able to use some portions of our service.
+      </p>
+      <h2 className="text-lg my-3">Amendments to the Privacy Policy</h2>
+      <p className="text-md font-sans font-light">
+        We may update our Privacy Policy from time to time. We will notify you of any changes by posting the new Privacy
+        Policy on this page.
+      </p>
+      <h2 className="text-lg my-3">Contact Information</h2>
+      <p className="text-md font-sans font-light">
+        For more information about our privacy practices, or if you have questions or concerns, please contact us via
+        email at {EMAIL} or by mail at PALIBATCHEE HOUSE s. r. o., with a registered seat at Klincova 35, 821 08
+        Bratislava, Slovak Republic.
+      </p>
+      <h2 className="text-lg my-3">Regulatory Authority</h2>
+      <p className="text-md font-sans font-light">
+        For any concerns regarding our use of your personal data, you can contact The Office for Personal Data
+        Protection of the Slovak Republic at Hraničná 12, 820 07 Bratislava, Slovak Republic.
       </p>
     </div>
   );
--- src/app/(side)/terms/page.tsx
+++ src/app/(side)/terms/page.tsx
@@ -3,16 +3,124 @@ import { ArrowBigLeft } from 'lucide-react';
 import Link from 'next/link';
 
 export default function TermsPage() {
+  const WEBSITE_NAME = 'Website Name';
+  const WEBSITE_ADDRESS = 'Website Address';
+
   return (
     <div className="p-10">
       <Link href="/" className={buttonVariants()}>
         <ArrowBigLeft className="w-6 h-6" />
         Back to home
       </Link>
-      <h1 className="text-2xl my-3">Terms and Conditions for Thumbnail Feedbacks</h1>
+      <h1 className="text-3xl my-3">Terms and Conditions</h1>
+      <p>Effective Date: 24/04/15</p>
       <p className="text-md font-sans font-light">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum amet accusantium et minus, quo ad velit
-        reprehenderit totam harum id. Quisquam quam, tempore assumenda fuga rerum aut. Odit, non earum!
+        Welcome to {WEBSITE_NAME}! These Terms and Conditions outline the rules and regulations for the use of{' '}
+        {WEBSITE_NAME}, located at {WEBSITE_ADDRESS}. By accessing this website, we assume you accept these terms and
+        conditions in full. Do not continue to use {WEBSITE_NAME} if you do not agree to all of the terms and conditions
+        stated on this page.
+      </p>
+      <h2 className="text-lg my-3">
+        1. The following terminology applies to these Terms and Conditions, Privacy Statement and Disclaimer Notice, and
+        any or all Agreements:
+      </h2>
+      <ul className="font-sans font-light list-disc ml-10">
+        <li>
+          <strong className="font-bold">&quot;Client&quot;, &quot;You&quot; and &quot;Your&quot;</strong> refers to you,
+          the person accessing this Website and accepting the company&apos;s terms and conditions.
+        </li>
+        <li>
+          <strong className="font-bold">
+            &quot;The Company&quot;, &quot;Ourselves&quot;, &quot;We&quot;, &quot;Our&quot; and &quot;Us&quot;
+          </strong>{' '}
+          refers to our company PALIBATCHEE HOUSE s. r. o., with a registered seat at Klincova 35, 821 08 Bratislava,
+          Slovak Republic, registered within the Commercial Register of Municipal Court Bratislava III.
+        </li>
+        <li>
+          <strong className="font-bold">&quot;Party&quot;, &quot;Parties&quot;, or &quot;Us&quot;</strong> refers to
+          both the Client and ourselves, or either the Client or ourselves.
+        </li>
+        <li>
+          <strong className="font-bold">&ldquo;Website&rdquo;</strong> refers to this website {WEBSITE_ADDRESS}.
+        </li>
+      </ul>
+      <h2 className="text-lg my-3">2. Use of the Website</h2>
+      <p className="text-md font-sans font-light">
+        On accessing this website, you warrant and represent to Us that you are legally entitled to do so and to make
+        use of information made available via the website.
+      </p>
+      <h2 className="text-lg my-3">3. Intellectual Property Rights</h2>
+      <p className="text-md font-sans font-light">
+        Other than content you own, which you may have opted to include on this Website, under these Terms, We and/or
+        Our licensors own all rights to the intellectual property and material contained in this Website, and all such
+        rights are reserved (“Website Material”).
+      </p>
+      <h2 className="text-lg my-3">4. Restrictions</h2>
+      <p className="text-md font-sans font-light">You are expressly restricted from the following:</p>
+
+      <ul className="list-disc ml-10 font-sans font-light">
+        <li>Publishig Website Material in any media</li>
+        <li>Selling, sublicensing and/or otherwise commercializing any Website Material</li>
+        <li>Publicly performing and/or showing any Website Material</li>
+        <li>Using this Website in any way that is, or may be, damaging to this Website</li>
+        <li>Using this Website in any way that impacts user access to this Website</li>
+        <li>
+          Using this Website contrary to applicable laws and regulations, or in a way that causes, or may cause, harm to
+          the Website, or to any person or business entity
+        </li>
+        <li>
+          Engaging in any data mining, data harvesting, data extracting, or any other similar activity in relation to
+          this Website, or while using this Website
+        </li>
+        <li>Using this Website to engage in any advertising or marketing</li>
+      </ul>
+      <h2 className="text-lg my-3">5. No warranties</h2>
+      <p className="text-md font-sans font-light">
+        This Website is provided &quot;as is,&quot; with all faults, and We make no express or implied representations
+        or warranties, of any kind related to this Website or the materials contained on this Website (including Website
+        Material).
+      </p>
+      <h2 className="text-lg my-3">6. Limitation of liability</h2>
+      <p className="text-md font-sans font-light">
+        In no event shall We, nor any of Our officers, directors, and employees, be liable for anything arising out of
+        or in any way connected with your use of this Website whether such liability is under contract, tort or
+        otherwise.
+      </p>
+      <h2 className="text-lg my-3">7. Indemnification</h2>
+      <p className="text-md font-sans font-light">
+        You hereby indemnify to the fullest extent Us from and against any and/or all liabilities, costs, demands,
+        causes of action, damages, and expenses arising in any way related to your breach of any of the provisions of
+        these Terms.
+      </p>
+      <h2 className="text-lg my-3">8. Severability</h2>
+      <p className="text-md font-sans font-light">
+        If any provision of these Terms is found to be unenforceable or invalid under any applicable law, such
+        unenforceability or invalidity shall not render these Terms unenforceable or invalid as a whole, and such
+        provisions shall be deleted without affecting the remaining provisions herein.
+      </p>
+      <h2 className="text-lg my-3">9. Variation of Terms</h2>
+      <p className="text-md font-sans font-light">
+        We are permitted to revise these terms at any time as it sees fit, and by using this Website you are expected to
+        review such Terms on a regular basis to ensure you understand all terms and conditions governing use of this
+        Website.
+      </p>
+      <h2 className="text-lg my-3">10. Assignment</h2>
+      <p className="text-md font-sans font-light">
+        We shall be permitted to assign, transfer, and subcontract its rights and/or obligations under these Terms
+        without any notification or consent required. However, you shall not be permitted to assign, transfer, or
+        subcontract any of your rights and/or obligations under these Terms
+      </p>
+      <h2 className="text-lg my-3">11. Entire Agreement</h2>
+      <p className="text-md font-sans font-light">
+        These Terms, including any legal notices and disclaimers contained on this Website, constitute the entire
+        agreement between Us and you in relation to your use of this Website, and supersede all prior agreements and
+        understandings with respect to the same.
+      </p>
+      <h2 className="text-lg my-3">12. Governing Law & Jurisdiction</h2>
+      <p className="text-md font-sans font-light">
+        These Terms will be governed by and construed in accordance with the laws of the Slovak Republic, and you submit
+        to the non-exclusive jurisdiction of the state and federal courts located in Slovak Republic for the resolution
+        of any disputes.
       </p>
     </div>
   );
