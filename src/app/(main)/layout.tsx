44fd6d268074df673b1f60e2f0d791a367f420a6
Pavol Vernarec <pver@paymorrow.com>
2024-04-21 11:27:26 +0200

    Root layout

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,41 +1,24 @@
-import type { Metadata } from 'next';
 import { Poppins } from 'next/font/google';
 import '../globals.css';
 import Navbar from '@/components/Navbar';
 import { Toaster } from '@/components/ui/toaster';
 import { Footer } from '@/components/Footer';
-import Scroll from '@/components/Scroll';
-import PlausibleProvider from 'next-plausible';
 
 const poppins = Poppins({ weight: '700', subsets: ['latin'] });
 
-export const metadata: Metadata = {
-  title: {
-    default: 'Thumbnails Feedback',
-    template: '%s | Thumbnails Feedback',
-  },
-  description: 'Get feedback before publishing your content and avoid mistakes that could cost you views.',
-};
-
-export default function RootLayout({
+export default function MainLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <head>
-        <PlausibleProvider domain="thumbnailsfeedback.com" />
-      </head>
-      <Scroll /> {/* Scroll to top of page when navigating to a new page */}
-      <body
-        className={`${poppins.className}  min-h-[calc(100vh - 200px)] bg-white text-slate-900 antialiased dark:bg-slate-900 dark:text-slate-50 w-full flex flex-col`}
-      >
-        <Navbar />
-        <main className="h-full">{children}</main>
-        <Toaster />
-        <Footer />
-      </body>
-    </html>
+    <body
+      className={`${poppins.className}  min-h-[calc(100vh - 200px)] bg-white text-slate-900 antialiased dark:bg-slate-900 dark:text-slate-50 w-full flex flex-col`}
+    >
+      <Navbar />
+      <main className="h-full">{children}</main>
+      <Toaster />
+      <Footer />
+    </body>
   );
 }
---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from 'next';
+import './globals.css';
+import PlausibleProvider from 'next-plausible';
+
+export const metadata: Metadata = {
+  title: {
+    default: 'Thumbnails Feedback',
+    template: '%s | Thumbnails Feedback',
+  },
+  description: 'Get feedback before publishing your content and avoid mistakes that could cost you views.',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+        <PlausibleProvider domain="thumbnailsfeedback.com" />
+      </head>
+      {children}
+    </html>
+  );
+}
