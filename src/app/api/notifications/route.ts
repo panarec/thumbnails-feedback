0bdd5abe4ae17f2e52eb44808dd9caf1bf5d2950
Pavol Vernarec <pver@paymorrow.com>
2024-05-08 12:36:10 +0200

    Filter tests for notifications which have 5 votes

--- src/app/api/notifications/route.ts
+++ src/app/api/notifications/route.ts
@@ -42,6 +42,15 @@ export async function GET(request: NextRequest) {
     return Response.json([]);
   }
 
+  const testsWithLessThan5votes = testsCreatedYesterday.filter((test) => {
+    const votes = test.thumbnails.reduce((acc, thumbnail) => {
+      return acc + thumbnail.votes.length;
+    }, 0);
+
+    return votes < 5;
+  });
+
+
   const allUsers = await db.user.findMany({
     select: {
       id: true,
@@ -55,7 +64,7 @@ export async function GET(request: NextRequest) {
   });
 
   let usersNotVotedOnAllTests = allUsers.filter((user) => {
-    return !testsCreatedYesterday.every((test) => {
+    return !testsWithLessThan5votes.every((test) => {
       return (
         test.thumbnails.some((thumbnail) => {
           return thumbnail.votes.some((vote) => vote.userId === user.id);
@@ -65,7 +74,7 @@ export async function GET(request: NextRequest) {
   });
 
   const usersNotVotedOnAllTestsAndVotesCount = usersNotVotedOnAllTests.map((user) => {
-    const userVotesNeeded = testsCreatedYesterday.filter((test) => {
+    const userVotesNeeded = testsWithLessThan5votes.filter((test) => {
       if (test.userId !== user.id)
         return !test.thumbnails.some((thumbnail) => {
           return thumbnail.votes.some((vote) => vote.userId === user.id);
