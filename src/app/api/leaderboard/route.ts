5c8b9c44e3cd110f6cbe03e422ce5e57de0c0d26
Pavol Vernarec <pver@paymorrow.com>
2024-05-22 11:07:52 +0200

    .

--- src/app/api/leaderboard/route.ts
+++ src/app/api/leaderboard/route.ts
@@ -1,7 +1,8 @@
 import { db } from '@/lib/db';
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 
-export async function GET(req: NextRequest) {
+export async function GET(req: Request) {
+  console.log(req)
   const usersWithMostCommentsAndVotes = await db.user.findMany({
     select: {
       comments: {
