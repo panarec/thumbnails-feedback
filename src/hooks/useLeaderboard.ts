ba56d62911eb5d540d8c29617ba42d28d44ded8a
Pavol Vernarec <pver@paymorrow.com>
2024-05-22 07:49:45 +0200

    test fetching leaderboard

--- src/hooks/useLeaderboard.ts
+++ src/hooks/useLeaderboard.ts
@@ -1,13 +1,15 @@
 import useSWR from 'swr';
 
 export const useLeaderboard = () => {
-  const fetcher = (url: string) =>
-    fetch(url).then((res) => {
+  const fetcher = (url: string) => {
+    console.log('fetching');
+    return fetch(url).then((res) => {
       if (res.status === 404) {
         throw new Error('Leaderboard not found');
       }
       return res.json();
     });
+  };
   const { data, error, isLoading } = useSWR(`/api/leaderboard`, fetcher);
 
   return {
